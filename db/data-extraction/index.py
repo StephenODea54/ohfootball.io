806992fbdc4945de82cadf85c03ba0c4461f23e1
Stephen.Odea <Stephen.Odea@medmutual.com>
2023-07-30 20:11:22 -0400

    Add get_team_schedule_links method

--- db/data-extraction/index.py
+++ db/data-extraction/index.py
@@ -1,13 +1,34 @@
-from typing import List
+# import logging
 from scraper.scraper import Scraper
 
 def main():
+    # logging.basicConfig(
+    #     level=logging.BASIC_FORMAT,
+    #     format="%(asctime)s %(levelname)s %(message)s",
+    #     datefmt="%Y-%m-%d %H:%M:%S",
+    #     filename="basic.log",
+    # )
+
     scraper = Scraper()
 
     # Get list of all season homepages
     season_homepage_links = scraper.get_season_homepage_links()
-    print(season_homepage_links)
+    
+    # Loop through each season's homepage and scrape the data
+    # If you wanted to add a logger, here is a good chance to use it!
+    for season_homepage_link in season_homepage_links:
+        scraper.update_url(season_homepage_link)
 
+        season = scraper.url[-4 : ]
+        print(f'Now scraping team schedule links for season {season}')
+        team_schedule_links = scraper.get_team_schedule_links(season)
+        print(team_schedule_links[:5])
+        print('')
+        
+            
+        # dataframe_constructor = Dataframeconstructor(scraper, team_schedule_links)
+        # team_dfs.append(dataframe_constructor.construct_team_df())
+        # schedule_dfs.append(dataframe_constructor.construct_schedule_df())
 
 if __name__ == '__main__':
     main()
--- db/data-extraction/scraper/scraper.py
+++ db/data-extraction/scraper/scraper.py
@@ -1,6 +1,7 @@
-from typing import List
+from typing import List, Type
 from bs4 import BeautifulSoup
 import requests
+
 
 class Scraper(BeautifulSoup):
     """
@@ -63,3 +64,84 @@ class Scraper(BeautifulSoup):
                 season_homepage_links.append(season_homepage_link)
         
         return season_homepage_links
+    
+    def get_team_schedule_links(self, season: str) -> List[str]:
+        """
+        Function that returns the urls of each team's schedule.
+        
+        Args:
+            season (str): The season for which we want to get the
+                          team schedules from.
+        
+        Returns:
+            team schedule links (List[str]): A list of the team schedule urls.
+        """
+
+        if season == '2000':
+            return simple_team_schedule_extractor(self)
+        else:
+            return complex_team_schedule_extractor(self)
+
+
+def simple_team_schedule_extractor(scraper: Type[Scraper]) -> List[str]:
+    """
+    Function that returns a list of each team's schedule url. This is considered
+    the "simple" extractor because, for instance, the team schedules for the 2000
+    season all exist on one url.
+
+    Args:
+        scraper (Scraper): Scraper object.
+    
+    Returns:
+        team schedule links (List[str]): A list of urls for each team's schedule.
+    """
+
+    anchor_tag_link = scraper.find('a')['href']
+    scraper.update_url(anchor_tag_link)
+
+    team_schedule_links: List[str] = []
+    team_schedule_anchor_tags = scraper.find_all('a')
+
+    for team_schedule_anchor_tag in team_schedule_anchor_tags:
+        team_schedule_link = team_schedule_anchor_tag['href']
+        team_schedule_links.append(team_schedule_link)
+    
+    return team_schedule_links
+
+def complex_team_schedule_extractor(scraper: Type[Scraper]) -> List[str]:
+    """
+    Function that returns a list of each team's schedule url. This is considered
+    the "complex" extractor because, for instance, the team schedules for the 2001
+    season exist on a variety of different urls.
+
+    Args:
+        scraper (Scraper): Scraper object.
+
+    Returns:
+        team schedule links (List[str]): A list of urls for each team's schedule.
+    """
+    region_homepage_links: List[str] = []
+    region_table = scraper.find('table', class_ = 'rankings')
+    region_anchor_tags = region_table.find_all('a')
+
+    for region_anchor_tag in region_anchor_tags:
+        region_homepage_link = region_anchor_tag['href']
+        region_homepage_links.append(region_homepage_link)
+    
+    team_schedule_links: List[str] = []
+    
+    for region_homepage_link in region_homepage_links:
+        scraper.update_url(region_homepage_link)
+
+        # Instead of parsing with a bunch of classes that change, the regions
+        # table is always the second one.
+        region_leaderboard_table = scraper.find_all('table')[1]
+        team_schedule_anchor_tags = region_leaderboard_table.find_all('a')
+
+        for team_schedule_anchor_tag in team_schedule_anchor_tags:
+            # Some links include the full url, some don't. To keep the navigation
+            # functionality of the scraper consistent, we are going to remove the base url part.
+            team_schedule_link = team_schedule_anchor_tag['href'].replace('http://www.joeeitel.com/hsfoot/', '')
+            team_schedule_links.append(team_schedule_link)
+    
+    return team_schedule_links
